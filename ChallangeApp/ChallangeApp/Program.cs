d621239bc9334a7a572e62870bc6c2d251832d4f
Konrad Walczak <konrad.walczak.99@gmail.com>
2024-01-17 03:25:42 +0100

    test code

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Konrad";
+var sex = "M";
+var age = 15;
+
+if (sex == "K" && age < 30)
+{
+    Console.WriteLine("Women under 30");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, 30 years old");
+}
+else if (sex == "M" && age<18)
+{
+    Console.WriteLine("Underaged male");
+}

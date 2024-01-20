369d2d007edbe85d116ac5069dd812bd7693f6fa
Konrad Walczak <konrad.walczak.99@gmail.com>
2024-01-20 07:21:04 +0100

    Zadanie domowe dzien 6

---
+++ ChallangeApp/ChallangeApp/Employee.cs
@@ -0,0 +1,32 @@
+﻿namespace ChallangeApp
+{
+    public class Employee
+    {
+        private int age;
+        private List<int> pointsEmployee = new List<int>();
+
+        public Employee(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.age = age;
+        }
+
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+
+        public int Points
+        {
+            get
+            {
+                return this.pointsEmployee.Sum();
+            }
+        }
+
+
+        public void AddPoints(int number)
+        {
+            this.pointsEmployee.Add(number);
+        }
+    }
+}
--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,13 +1,41 @@
 ﻿using ChallangeApp;
 
-User user1 = new User("Adam", "haselko2137");
-User user2 = new User("Monika", "haslo123");
-User user3 = new User("Zuzia", "3232321313");
-User user4 = new User("Damian", "123");
+Employee employee1 = new Employee("Jan", "Kowalski", 33);
+Employee employee2 = new Employee("Magda", "Baklazan", 21);
+Employee employee3 = new Employee("Jacek", "Nowak", 28);
+AddingPoints();
+var bestEmployee = BestEmployee();
+Console.WriteLine("Najwięcej punktów otrzymał/a: " + bestEmployee.Name + " " + bestEmployee.Surname + ", a suma jego/jej punktów wyniosła: " + bestEmployee.Points); ;
 
-user1.AddScore(5);
-user1.AddScore(2);
-var result = user1.Result;
-Console.WriteLine(result);
+void AddingPoints()
+{
+    Random rand = new Random();
+    employee1.AddPoints(rand.Next(0, 10));
+    employee1.AddPoints(rand.Next(0, 10));
+    employee1.AddPoints(rand.Next(0, 10));
+    employee2.AddPoints(rand.Next(0, 10));
+    employee2.AddPoints(rand.Next(0, 10));
+    employee2.AddPoints(rand.Next(0, 10));
+    employee3.AddPoints(rand.Next(0, 10));
+    employee3.AddPoints(rand.Next(0, 10));
+    employee3.AddPoints(rand.Next(0, 10));
+}
+Employee BestEmployee()
+{
+    List<Employee> pointsOfEmployees = new List<Employee>()
+    {
+        employee1, employee2, employee3
+    };
+    int maxPoints = -1;
+    Employee bestEmployee = null;
 
-
+    foreach (var employee in pointsOfEmployees)
+    {
+        if (employee.Points > maxPoints)
+        {
+            bestEmployee = employee;
+            maxPoints = employee.Points;
+        }
+    }
+    return bestEmployee;
+}
